3fcf7ea5046c830256130924ae9490d94ccad557
Efua Akumanyi <efua.akumanyi@gmail.com>
2021-07-21 13:55:45 +0100

    Add full working project code

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import './styles/App.css';
+import Header from './components/Header';
+import BookList from './components/BookList';
+import Search from './components/Search';
+import About from './pages/About';
+import data from './models/local-books.json';
+
+const App = () => {
+
+  const [books, setBooks] = useState(data);
+  const [bookcase, setBookcase] = useState([]);
+  const [keyword, setKeyword] = useState('');
+
+  const addToBookcase = (id) => {
+    setBookcase(bookcase.concat(books.filter(book => book.id === id)));
+    setBooks([...books.map(book => {
+      if (book.id === id) {
+        book.read = true;
+      }
+      return book;
+    }
+    )]);
+  }
+
+  const removeFromBookcase = (id) => {
+    setBookcase(bookcase.filter(book => book.id !== id));
+    setBooks([...books.map(book => {
+      if (book.id === id) {
+        book.read = false;
+      }
+      return book;
+    }
+    )]);
+  }
+
+  useEffect(() => {
+    document.title = `My Library ${bookcase.length} Read`;
+    Array.from(document.getElementsByClassName("bookLink")).forEach((el) => {
+      el.innerText = ` Bookcase (${bookcase.length})`;
+    });
+  });
+
+  async function findBooks(value) {
+    const results = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${value}&filter=paid-ebooks&print-type=books&projection=lite`).then(res => res.json());
+    if (!results.error) {
+      setBooks(results.items.filter(result => bookcase.findIndex(book => result.id === book.id)===-1));
+    }
+  }
+  return (
+    <Router>
+      <div className="container">
+        <Route exact path="/" render={() => (
+          <Fragment>
+            <Header bookLength={bookcase.length} />
+            <Search keyword={keyword} findBooks={findBooks} setKeyword={setKeyword}/>
+            <BookList books={books} stored="library" addToBookcase={addToBookcase} removeFromBookcase={removeFromBookcase} />
+          </Fragment>
+        )} />
+        <Route path="/bookcase" render={() => (
+          <Fragment>
+            <Header bookLength={bookcase.length} />
+            <BookList books={bookcase} stored="bookcase" addToBookcase={addToBookcase} removeFromBookcase={removeFromBookcase} />
+          </Fragment>
+        )} />
+        <Route path="/about" component={() => <About bookLength={bookcase.length} />} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const propTypes = {};
+const defaultProps = {};
+
+const Search = (props) => {
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        props.findBooks(props.keyword);
+    };
+
+    return <div className="searchBar">
+        <form onSubmit={handleSubmit}>
+        <label>
+            <input type="text" placeholder="Enter name, author, keyword or genre..." name="search" value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)}/>
+        </label>
+        <input type="submit" value="Find"/>
+        </form>
+        <h1>{props.keyword && 'Searching for keyword:' + props.keyword}</h1>
+    </div>
+}
+
+Search.propTypes = propTypes;
+Search.defaultProps = defaultProps;
+// #endregion
+
+export default Search;
