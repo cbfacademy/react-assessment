c5ffa5032385206d343d79a7c6f53dd28146b0c4
Genyus <genyus@gmail.com>
2022-04-12 18:27:41 +0100

    test: update getByText to use partial match

--- src/__tests__/BasketTotal.spec.js
+++ src/__tests__/BasketTotal.spec.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import BasketTotal from '../components/BasketTotal';
-import { formatCurrency } from "../components/utils.js";
+import BasketTotal from "../components/BasketTotal";
 
 describe("BasketTotal functionality", () => {
   test("Displays correct basket total", () => {
     const basketTotal = 10;
     const { getByText } = render(<BasketTotal basketTotal={basketTotal} />);
-    expect(getByText(formatCurrency(basketTotal))).toBeInTheDocument();
+    expect(getByText(basketTotal, { exact: false })).toBeInTheDocument();
   });
 });
