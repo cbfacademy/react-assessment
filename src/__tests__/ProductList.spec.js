b978f0a641954fa5cc95f6414e695a4061f232e1
Genyus <genyus@gmail.com>
2023-01-15 04:04:49 +0000

    test: relaxes and formats tests

--- src/__tests__/ProductList.spec.js
+++ src/__tests__/ProductList.spec.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import ProductList from '../components/ProductList';
-import data from '../models/data.json';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ProductList from "../components/ProductList";
+import data from "../models/data.json";
 
 describe("ProductList functionality", () => {
-  test('Displays initial product list', () => {
+  test("Displays initial product list", () => {
     const expectedLength = data.length;
-    render(<ProductList items={data} addToBasket={null} removeFromBasket={null} itemCount={data.length} />);
-    expect(document.querySelectorAll('.product').length).toBe(expectedLength);
-    expect(document.querySelectorAll('.empty').length).toBe(0);
+    render(
+      <ProductList
+        items={data}
+        addToBasket={null}
+        removeFromBasket={null}
+        itemCount={data.length}
+      />
+    );
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll(".product").length).toBe(expectedLength);
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll(".empty").length).toBe(0);
   });
 
-  test('Displays empty product list', () => {
+  test("Displays empty product list", () => {
     const emptyData = [];
     const expectedLength = emptyData.length;
-    const { getByText } = render(<ProductList items={emptyData} addToBasket={null} removeFromBasket={null} itemCount={emptyData.length} />);
-    expect(document.querySelectorAll('.product').length).toBe(expectedLength);
-    expect(document.querySelectorAll('.empty').length).toBe(1);
-    expect(getByText('No items found...')).toBeInTheDocument();
+    render(
+      <ProductList
+        items={emptyData}
+        addToBasket={null}
+        removeFromBasket={null}
+        itemCount={emptyData.length}
+      />
+    );
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll(".product").length).toBe(expectedLength);
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(document.querySelectorAll(".empty").length).toBe(1);
+    expect(
+      screen.getByText("No items found", { exact: false })
+    ).toBeInTheDocument();
   });
 });
