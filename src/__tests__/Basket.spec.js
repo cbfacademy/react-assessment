1e4b08148a25e3c6b14ad3340bc436f4e95b3b37
Genyus <genyus@gmail.com>
2023-01-15 04:04:49 +0000

    test: relaxes and formats tests

--- src/__tests__/Basket.spec.js
+++ src/__tests__/Basket.spec.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Basket from '../components/Basket';
-import data from '../models/data.json';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Basket from "../components/Basket";
+import data from "../models/data.json";
 
 describe("Basket component functionality", () => {
-  test('Displays empty basket', () => {
+  test("Displays empty basket", () => {
     const emptyBasket = [];
     const expectedLength = emptyBasket.length;
     const basketCount = 0;
@@ -24,11 +24,11 @@ describe("Basket component functionality", () => {
     // eslint-disable-next-line testing-library/no-node-access
     expect(document.querySelectorAll(".empty").length).toBe(1);
     expect(
-      screen.getByText("Sorry, no items in basket...")
+      screen.getByText("no items found", { exact: false })
     ).toBeInTheDocument();
   });
 
-  test('Displays populated basket', () => {
+  test("Displays populated basket", () => {
     const basket = data.slice(0, 3);
     const expectedLength = basket.length;
     const basketCount = basket.length;
@@ -48,11 +48,19 @@ describe("Basket component functionality", () => {
     expect(document.querySelectorAll(".empty").length).toBe(0);
   });
 
-  test('calls removeFromBasket prop when clicked', async () => {
+  test("calls removeFromBasket prop when clicked", async () => {
     const removeFromBasket = jest.fn();
-    const products = data.slice(0, 1).map(item => ({ ...item, inBasket: true }));
-    render(<Basket basket={products} removeFromBasket={removeFromBasket} basketCount={products.length} />);
-    const button = screen.getByRole('button');
+    const products = data
+      .slice(0, 1)
+      .map((item) => ({ ...item, inBasket: true }));
+    render(
+      <Basket
+        basket={products}
+        removeFromBasket={removeFromBasket}
+        basketCount={products.length}
+      />
+    );
+    const button = screen.getByRole("button");
     fireEvent.click(button);
     expect(removeFromBasket).toHaveBeenCalledTimes(1);
   });
